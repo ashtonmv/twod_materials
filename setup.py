eedc3258a339e44f4651313b0f8ec6997e060d3c
Michael Ashton <mashton@mseadmins-iMac.local>
2016-12-12 13:53:48 -0500

    Add manifest.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'twod_materials',
   packages = find_packages(),
-  version = '1.0.0',
+  version = '1.0.1',
   install_requires = ['monty>=0.7.2', 'matplotlib>=1.4.2', 'nose>=1.3',
                       'scipy==0.14.0', 'pymatgen==4.2.0'],
   extras_require = {'doc': ['codecov>=2.0', 'sphinx>=1.3.1']},
@@ -13,5 +13,5 @@ setup(
   author = 'Michael Ashton',
   author_email = 'mashton@ufl.edu',
   url = 'https://github.com/ashtonmv/twod_materials',
-  download_url = 'https://github.com/ashtonmv/twod_materials/tarball/1.0.0',
+  download_url = 'https://github.com/ashtonmv/twod_materials/tarball/1.0.1',
 )
